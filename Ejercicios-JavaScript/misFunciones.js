b47106a7262cd2e140f604878fed7c5a94d3f572
Juani135 <juaniviel10@hotmail.com>
2022-06-13 20:05:57 -0300

    Se realizo ejercicio Operaciones Matematicas II

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -59,7 +59,7 @@ function calcularSuma(){
 
     num1=Number(document.getElementsByName("sum_num1")[0].value);
     num2=document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value= num1 + Number(num2);
+    document.getElementsByName("sum_total")[0].innerHTML= num1 + Number(num2);
 }
 
 function calcularResta(){
@@ -67,7 +67,7 @@ function calcularResta(){
 
     num1=Number(document.getElementsByName("res_num1")[0].value);
     num2=document.getElementsByName("res_num2")[0].value;
-    document.getElementsByName("res_total")[0].value= num1 - Number(num2);
+    document.getElementsByName("res_total")[0].innerHTML= num1 - Number(num2);
 }
 
 function calcularMult(){
@@ -75,7 +75,7 @@ function calcularMult(){
 
     num1=Number(document.getElementsByName("mul_num1")[0].value);
     num2=document.getElementsByName("mul_num2")[0].value;
-    document.getElementsByName("mul_total")[0].value= num1 * Number(num2);
+    document.getElementsByName("mul_total")[0].innerHTML= num1 * Number(num2);
 }
 
 function calcularDiv(){
@@ -83,5 +83,5 @@ function calcularDiv(){
 
     num1=Number(document.getElementsByName("div_num1")[0].value);
     num2=document.getElementsByName("div_num2")[0].value;
-    document.getElementsByName("div_total")[0].value= num1 / Number(num2);
+    document.getElementsByName("div_total")[0].innerHTML= num1 / Number(num2);
 }
